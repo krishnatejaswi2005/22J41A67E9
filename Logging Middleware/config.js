6ee15c963bde1aa15f661cff34021492e2a2a9e8
krishnatejaswi2005 <tejaswi.jgs@gmail.com>
2025-07-09 11:55:13 +0530

    Created file structure and Logging Middleware

---
+++ Logging Middleware/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	LOG_API_URL: "http://20.244.56.144/eva1uation-service/logs",
+};
---
+++ Logging Middleware/logger.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const { LOG_API_URL } = require("./config");
+
+const allowedStacks = ["backend", "frontend"];
+const allowedLevels = ["debug", "info", "warn", "error", "fatal"];
+const allowedPackages = {
+	backend: [
+		"cache",
+		"controller",
+		"cron_job",
+		"db",
+		"domain",
+		"handler",
+		"repository",
+		"route",
+		"service",
+	],
+	frontend: ["api", "component", "hook", "page", "state", "style"],
+	shared: ["auth", "config", "middleware", "utils"],
+};
+
+async function log(stack, level, pkg, message) {
+	try {
+		if (!allowedStacks.includes(stack)) {
+			throw new Error(`Invalid stack: ${stack}`);
+		}
+
+		if (!allowedLevels.includes(level)) {
+			throw new Error(`Invalid level: ${level}`);
+		}
+
+		const isValidPackage =
+			allowedPackages[stack].includes(pkg) ||
+			allowedPackages.shared.includes(pkg);
+		if (!isValidPackage) {
+			throw new Error(`Invalid package '${pkg}' for stack '${stack}'`);
+		}
+
+		const response = await axios.post(LOG_API_URL, {
+			stack,
+			level,
+			package: pkg,
+			message,
+		});
+
+		console.log("Log success:", response.data);
+		return response.data;
+	} catch (error) {
+		console.error("Logging failed:", error.message);
+	}
+}
+
+module.exports = { log };
